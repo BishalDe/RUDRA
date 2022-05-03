258ac2aff987bd33611e8c785b764087ac23e952
BishalDe <itsbishalde@gmail.com>
2022-05-03 22:39:00 +0530

    Added package
    added a module and a package

---
+++ rudra/rudra.py
@@ -0,0 +1,57 @@
+"""
+Created by bishal on 28-04-2022
+The module name is rudrainfo - To import it just type 'import rudrainfo'
+This module shows information about the RUDRA Team.
+"""
+
+
+def rudra():
+    message="""
+    RUDRA - SRM MARS ROVER
+    Science, Technology & Engineering
+    Innovate 💡 Design 🖥️ Build 👷 Repeat 🔄
+    Mars Rover research team of @srmuniversityofficial, Chennai, India.
+    linktr.ee/team_rudra
+
+     RUDRA is the official team of SRM IST taking part in University Rover Challenge organised by Mars
+     Society since 2013. The annual competition is held at the Mars Desert Research Station (MDRS), 
+     Martian Analog Site, near Hanksville, Utah, USA. The team has been successfully operational for the 
+     last 7 years and has maintained its consistency in the competition.
+     Our Team consists of 25-30 undergraduate students of every year from various engineering streams,
+     working in different domains. The fun part - R&D, is the soul and essence of Team Rudra. 
+     Brainstorming discussions are always welcome for giving birth to new ideas.
+    
+
+    -The core of Team Rudra believes in giving an opportunity to everyone.
+
+
+    #INDIAN ROVER CHALLENGE :-)
+
+    Indian Rover Challenge is the only robotics and space exploration competition of its kind in 
+    Asia-Pacific which challenges student teams to design and build next- generation Mars rovers. It is 
+    part of the 'Rover Challenge Series' of 'The Mars Society', USA.
+    Team RUDRA achieved 3rd global and 1st rank among the Asian teams in IRC2020.
+
+
+    UNIVERSITY ROVER CHALLENGE :-)
+
+    The University Rover Challenge (URC) is the world's premier robotics competition for college
+     students. Held annually in the desert of southern Utah in the United States, URC challenges student
+     teams to design and build the next generation of Mars rovers that will one day work alongside 
+     astronauts explorin the Red Planet.
+    URC was launched in 2006, with competitions being held every summer since 2007. URC consistently 
+    draws an international field of the most talented an promising students around.
+
+    Team RUDRA achieved 11th global and 2nd rank among the Asian teams in URC2019
+
+
+    YOUTUBE :- https://www.youtube.com/channel/UC2Lu4ABOxd43mizJGFATF_w
+    INSTAGRAM :- https://www.instagram.com/team_rudra/
+    FACEBOOK :- https://www.facebook.com/srmteamrudra
+    TWITTER :- https://twitter.com/srmrudra
+    """
+
+    print(message)
+
+
+rudra()

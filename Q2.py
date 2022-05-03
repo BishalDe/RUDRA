e317140524e6398fc8853bcedb4d24edfba7427d
BishalDe <itsbishalde@gmail.com>
2022-05-03 11:44:45 +0530

    github pushing

--- Q2.py
+++ Q2.py
@@ -2,7 +2,7 @@
 Here there are two functions that uses the principle of *args & **kargs to print your name...!
 FUNCTION- print_name() is for *args & print_kname() is for **kargs
 """
-
+import rudrainfo
 
 def print_name(*args):
     print("Hello "+args[0]+" "+args[1]+"...!")
